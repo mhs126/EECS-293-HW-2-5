b7626f0b25e88250682a7bd5d085b55863edd6e0
Michael Silverman <44821540+mhs126@users.noreply.github.com>
2019-02-12 22:20:19 -0500

    Add files via upload

--- NonTerminalSymbol.java
+++ NonTerminalSymbol.java
@@ -1,5 +1,7 @@
 package Parser;
 
+import com.sun.org.apache.xpath.internal.operations.Minus;
+
 import java.util.*;
 
 import static java.util.Optional.empty;
@@ -10,64 +12,74 @@ enum NonTerminalSymbol implements Symbol {
     //Non-Terminal types
     EXPRESSION, EXPRESSION_TAIL, TERM, TERM_TAIL, UNARY, FACTOR;
 
-    static Map<NonTerminalSymbol, List<SymbolSequence>> nonTerminalSymbolsMap = new HashMap<>();
+    /*
+    Creates the map with NonTerminalSymbol key and map value
+    Creates six maps, one for each NonTerminal with a TerminalSymbol key and SymbolSequence value
+     */
+    private static Map<NonTerminalSymbol, Map<TerminalSymbol, SymbolSequence>> nonTerminalSymbolsMap = new HashMap<>();
+
+    private static Map<TerminalSymbol, SymbolSequence> expressionMap = new HashMap<>();
+
+    private static Map<TerminalSymbol, SymbolSequence> expression_tailMap = new HashMap<>();
+
+    private static Map<TerminalSymbol, SymbolSequence> termMap = new HashMap<>();
+
+    private static Map<TerminalSymbol, SymbolSequence> term_tailMap = new HashMap<>();
+
+    private static Map<TerminalSymbol, SymbolSequence> unaryMap = new HashMap<>();
+
+    private static Map<TerminalSymbol, SymbolSequence> factorMap = new HashMap<>();
 
     static {
         //Creates the list for expression
-        List<SymbolSequence> expressionList = new ArrayList<>();
         SymbolSequence expression1 = SymbolSequence.build(TERM, EXPRESSION_TAIL);
-        expressionList.add(expression1);
-        nonTerminalSymbolsMap.put(EXPRESSION, expressionList);
+        expressionMap.put(TerminalSymbol.VARIABLE, expression1);
+        expressionMap.put(TerminalSymbol.OPEN, expression1);
+        expressionMap.put(TerminalSymbol.MINUS, expression1);
+        nonTerminalSymbolsMap.put(EXPRESSION, expressionMap);
 
         //Creates the list for expression_tail
-        List<SymbolSequence> expression_tailList = new ArrayList<>();
         SymbolSequence expression_tail1 = SymbolSequence.build(TerminalSymbol.PLUS, TERM, EXPRESSION_TAIL);
         SymbolSequence expression_tail2 = SymbolSequence.build(TerminalSymbol.MINUS, TERM, EXPRESSION_TAIL);
-        expression_tailList.add(expression_tail1);
-        expression_tailList.add(expression_tail2);
-        expression_tailList.add(SymbolSequence.EPSILON);
-        nonTerminalSymbolsMap.put(EXPRESSION_TAIL, expression_tailList);
+        expression_tailMap.put(TerminalSymbol.PLUS, expression_tail1);
+        expression_tailMap.put(TerminalSymbol.MINUS, expression_tail2);
+        expression_tailMap.put(null, SymbolSequence.EPSILON);
+        nonTerminalSymbolsMap.put(EXPRESSION_TAIL, expression_tailMap);
 
         //Creates the list for term
-        List<SymbolSequence> termList = new ArrayList<>();
         SymbolSequence term1 = SymbolSequence.build(UNARY, TERM_TAIL);
-        termList.add(term1);
-        nonTerminalSymbolsMap.put(TERM, termList);
+        termMap.put(TerminalSymbol.MINUS, term1);
+        termMap.put(TerminalSymbol.VARIABLE, term1);
+        nonTerminalSymbolsMap.put(TERM, termMap);
 
         //Creates the list for term_list
-        List<SymbolSequence> term_tailList = new ArrayList<>();
         SymbolSequence term_tail1 = SymbolSequence.build(TerminalSymbol.TIMES, UNARY, TERM_TAIL);
         SymbolSequence term_tail2 = SymbolSequence.build(TerminalSymbol.DIVIDE, UNARY, TERM_TAIL);
-        term_tailList.add(term_tail1);
-        term_tailList.add(term_tail2);
-        term_tailList.add(SymbolSequence.EPSILON);
-        nonTerminalSymbolsMap.put(TERM_TAIL, term_tailList);
+        term_tailMap.put(TerminalSymbol.TIMES, term_tail1);
+        term_tailMap.put(TerminalSymbol.DIVIDE, term_tail2);
+        nonTerminalSymbolsMap.put(TERM_TAIL, term_tailMap);
 
         //creates the list for unary
-        List<SymbolSequence> unaryList = new ArrayList<>();
         SymbolSequence unary1 = SymbolSequence.build(TerminalSymbol.MINUS, FACTOR);
         SymbolSequence unary2 = SymbolSequence.build(FACTOR);
-        unaryList.add(unary1);
-        unaryList.add(unary2);
-        nonTerminalSymbolsMap.put(UNARY, unaryList);
+        unaryMap.put(TerminalSymbol.MINUS, unary1);
+        unaryMap.put(TerminalSymbol.VARIABLE, unary2);
+        nonTerminalSymbolsMap.put(UNARY, unaryMap);
 
         //creates list for factor
-        List<SymbolSequence> factorList = new ArrayList<>();
         SymbolSequence factor1 = SymbolSequence.build(TerminalSymbol.OPEN, EXPRESSION, TerminalSymbol.CLOSE);
         SymbolSequence factor2 = SymbolSequence.build(TerminalSymbol.VARIABLE);
-        factorList.add(factor1);
-        factorList.add(factor2);
-        nonTerminalSymbolsMap.put(FACTOR, factorList);
+        factorMap.put(TerminalSymbol.OPEN, factor1);
+        factorMap.put(TerminalSymbol.VARIABLE, factor2);
+        nonTerminalSymbolsMap.put(FACTOR, factorMap);
     }
 
     public ParseState parse(List<Token> list) {
-        for (SymbolSequence symbolSequence : nonTerminalSymbolsMap.get(this)) {
-            ParseState p = symbolSequence.match(Objects.requireNonNull(list,
+        SymbolSequence parseSequence = nonTerminalSymbolsMap.get(this).get(list.get(0).getType());
+            ParseState p = parseSequence.match(Objects.requireNonNull(list,
                     "Input list is null, please enter a valid list"));
             if (p.isSuccess()) {
                 return p;
-            }
-            //else it is not successful
 
         }
         return ParseState.FAILURE;
