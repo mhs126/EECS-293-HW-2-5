768fbe8738badeeb5169475b4b6f429c03ceccf1
Michael Silverman <44821540+mhs126@users.noreply.github.com>
2019-04-16 19:52:07 -0400

    Update Type.java

--- Type.java
+++ Type.java
@@ -1,25 +1,30 @@
 public class Type {
 
   public String name;
-  public final List<Type> higherList = new ArrayList<>();
-  public final List<Type> lowerList = new ArrayList<>();
-  public final List<Type? compatable = new ArrayList<>();
-  public final List<Connectors> validConnectors=  new ArrayList<>();
+  public final Set<Type> higherSet = new HashSet<>();
+  public final Set<Type> lowerSet = new HashSet<>();
+  public final Set<Type? compatable = new HashSet<>();
+  public final Set<Connectors> validConnectors=  new HashSet<>();
   
-  public Type(String name, List<String> higher, List<String> lower){
+  public Type(String name, Set<Type> higher, Set<Type> lower){
     this.name = name;
-    this.higher = higherList;
-    this.lower = lowerList;
-    this.compatable = new ArrayList<Type>(lowerList);
-    this.compatable.addAll(higherList);
-    List<Connectors> tempList = new ArrayList<>();
-    tempList.addAll(Connector.PLUS, Connector.MINUS, Connector.TIMES
+    this.higherSet = higher;
+    this.lowerSet = lower;
+    this.compatable = new HashSet<Type>(lower);
+    this.compatable.addAll(higher);
+    Set<Connectors> tempSet = new HashSet<>();
+    tempSet.addAll(Connector.PLUS, Connector.MINUS, Connector.TIMES
                                 Connector.DIVIDE, Connector.OPEN, Connector.CLOSE);
-    this.validConnectors = tempList;
+    this.validConnectors = tempSet;
   }
   
   @override
   public String toString(){
     return  name;
     }
+  
+  public boolean isValidOperator(Connector c){
+     return validConnectors.contains(c);
+  }
+  
 }
