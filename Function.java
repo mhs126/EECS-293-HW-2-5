04e3ce349bd6d663add002f28418b734eab23970
Michael Silverman <44821540+mhs126@users.noreply.github.com>
2019-04-16 19:45:55 -0400

    Update and rename Function to Function.java

---
+++ Function.java
@@ -0,0 +1,26 @@
+public class Function extends Type {
+
+  public String name;
+  public final List<Type> higherList = new ArrayList<>();
+  public final List<Type> lowerList = new ArrayList<>();
+  public final List<Type? compatable = new ArrayList<>();
+  public final List<Connectors> validConnectors=  new ArrayList<>();
+  
+  public Type(String name, List<String> higher, List<String> lower){
+    this.name = name;
+    this.higher = higherList;
+    this.lower = lowerList;
+    this.compatable = new ArrayList<Type>(lowerList);
+    this.compatable.addAll(higherList);
+    List<Connectors> tempList = new ArrayList<>();
+    tempList.addAll(Connector.PLUS, Connector.TIMES);
+    this.validConnectors = tempList;
+  }  
+  
+  @override
+  public String toString(){
+    return name;
+  }
+
+
+}
