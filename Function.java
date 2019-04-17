cf242da7666d98f6b4a7a07e3ceeb41a1b46964d
Michael Silverman <44821540+mhs126@users.noreply.github.com>
2019-04-16 20:36:12 -0400

    Update Function.java

--- Function.java
+++ Function.java
@@ -23,24 +23,38 @@ public class Function extends Type {
     //this.validConnectors = tempSet;
   }
   
-  @override
+  //Overrides the toString method
+  //Returns the name of the type
+  @Override
   public String toString(){
     return  name;
     }
-  
-  public boolean isValidOperator(Connector c){
+  /*
+  INPUT: TerminalSymbol 
+  returns true if the input is in the validConnectors set
+  */
+  public boolean isValidOperator(TerminalSymbol c){
      return validConnectors.contains(c);
   }
   
+  //Public build method for Function
   public Function Build(String name, Set<Type> higher, Set<Type> lower){
     return new Type(name, higher, lower); 
   }
   
+  /*
+  INPUT: Type
+  adds the input Type to the higher and compatible sets
+  */
   public void addToHigher(Type t){
     this.higherSet.add(t);
     this.compatible.add(t);
   }
   
+    /*
+  INPUT: Type
+  adds the input Type to the lower and compatible sets
+  */
   public void addToLower(Type t){
     this.lowerSet.add(t);
     this.compatible.add(t);
