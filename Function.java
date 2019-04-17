2ab6660d53bd3d68bcdf5ff4a26858f9104e9516
Michael Silverman <44821540+mhs126@users.noreply.github.com>
2019-04-16 20:25:19 -0400

    Update Function.java

--- Function.java
+++ Function.java
@@ -1,18 +1,23 @@
 public class Function extends Type {
 
   public String name;
-  public final Set<Type> higherSet;
-  public final Set<Type> lowerSet;
-  public final Set<Type> compatible;
-  public final Set<TerminalSymbol> validConnectors;
+  public Set<Type> higherSet;
+  public Set<Type> lowerSet;
+  public Set<Type> compatible;
+  public Set<TerminalSymbol> validConnectors;
   
   private Type(String name, Set<Type> higher, Set<Type> lower){
     this.name = name;
     this.higherSet = higher;
     this.lowerSet = lower;
+    this.lowerSet.add(this)
     this.compatable = new HashSet<Type>(lower);
     this.compatable.addAll(higher);
-    this.validConnectors = new HashSet<Type>().addAll(Stream.of(TerminalSymbol.PLUS, TerminalSymbol.TIMES).collect.Collectors.toSet()));
+    this.validConnectors = new HashSet<Type>().addAll(
+                                              Stream.of(TerminalSymbol.PLUS, TerminalSymbol.TIMES)
+                                              .collect
+                                              .Collectors
+                                              .toSet()));
     //Set<Connector> tempSet = new HashSet<>();
     //tempSet.addAll(Stream.of(TerminalSymbol.PLUS, TerminalSymbol.TIMES).collect.Collectors.toSet()));
     //this.validConnectors = tempSet;
@@ -29,6 +34,16 @@ public class Function extends Type {
   
   public Function Build(String name, Set<Type> higher, Set<Type> lower){
     return new Type(name, higher, lower); 
+  }
+  
+  public void addToHigher(Type t){
+    this.higherSet.add(t);
+    this.compatible.add(t);
+  }
+  
+  public void addToLower(Type t){
+    this.lowerSet.add(t);
+    this.compatible.add(t);
   }
   
 }
